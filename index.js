fac513c17b14e3fbe33cc00b5bb2999db5a87119
Tamim Mahmud <99955798+tamim-jr@users.noreply.github.com>
2022-06-12 21:12:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ if (hddSpace == true && ssdSpace >4000){
 else{
     console.log("I don't buy this pc");
 }
+
+var corei5 = false;
+var clockSpeed = 4.20;
+if (corei5 == true || clockSpeed > 4){
+    console.log("I want to buy this Computer");
+}
+else{
+    console.log("I don't buy this pc");
+}
+
+var corei7 = true;
+var clockSpeed = 3.20;
+if (corei7 == true || clockSpeed > 4){
+    console.log("I want to buy this Computer");
+}
+else{
+    console.log("I don't buy this pc");
+}
