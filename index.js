f960da31cd3a03dfe43804fa0fc6abc5c572c7b0
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-12 21:01:25 +0600

    Multiple condition in javaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+var hddSpace = true;
+var ssdSpace = 4000;
+if (hddSpace == true && ssdSpace >4000){
+    console.log("I want to buy this Computer");
+}
+else{
+    console.log("I don't buy this pc");
+}
